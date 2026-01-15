426fc85db7ceee29b8ead81e7a3312c590ad0580
Student-2024 <student-2024@ashalim.org>
2026-01-15 11:47:49 +0200

    $$$$$####

--- app/src/main/java/com/example/finalproj/UserProfile.java
+++ app/src/main/java/com/example/finalproj/UserProfile.java
@@ -1,5 +1,6 @@
 package com.example.finalproj;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
@@ -10,6 +11,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.finalproj.model.User;
+import com.example.finalproj.services.DatabaseService;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,10 +20,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class UserProfile extends AppCompatActivity {
-    private String uid;
-    private FirebaseAuth mAuth;
+import org.w3c.dom.Text;
 
+public class UserProfile extends BaseActivity {
+    private TextView userName, phoneNumber, mail;
+    private String uid, fullName, phoneNum, email;
+    private FirebaseAuth mAuth;
+    private DatabaseService databaseService;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,9 +40,28 @@ public class UserProfile extends AppCompatActivity {
             return insets;
         });
         mAuth = FirebaseAuth.getInstance();
+        databaseService = DatabaseService.getInstance();
         uid = mAuth.getCurrentUser().getUid();
+        userName = findViewById(R.id.txtUserNameProfile);
+        phoneNumber = findViewById(R.id.txtUserPhoneProfile);
+        mail = findViewById(R.id.txtUserEmailProfile);
+        databaseService.getUser(uid, new DatabaseService.DatabaseCallback<User>() {
+            @Override
+            public void onCompleted(User object) {
+                fullName = object.getfName() + " " + object.getlName();
+                phoneNum = object.getPhone();
+                email = object.getEmail();
 
+                userName.setText(fullName);
+                phoneNumber.setText(phoneNum);
+                mail.setText(email);
+            }
 
+            @Override
+            public void onFailed(Exception e) {
+
+            }
+        });
     }
 
 }
