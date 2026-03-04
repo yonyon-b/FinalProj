126c72ed17ca85bc069bf286becdeec63c034d9e
YonYon <bar.jntn@gmail.com>
2026-03-04 18:48:03 +0200

    search bar working

--- app/src/main/java/com/example/finalproj/ItemList.java
+++ app/src/main/java/com/example/finalproj/ItemList.java
@@ -15,6 +15,7 @@ import com.example.finalproj.model.Item;
 import com.example.finalproj.model.ItemRecyclerAdapter;
 import com.example.finalproj.services.DatabaseService;
 
+import androidx.appcompat.widget.SearchView;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,8 +23,10 @@ public class ItemList extends BaseActivity {
 
     private RecyclerView recyclerView;
     private ArrayList<Item> dataList = new ArrayList<>();
+    private ArrayList<Item> allItems = new ArrayList<>();
     private ItemRecyclerAdapter adapter;
     private DatabaseService databaseService;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +39,7 @@ public class ItemList extends BaseActivity {
             return insets;
         });
         recyclerView = findViewById(R.id.rvItems);
+        searchView = findViewById(R.id.searchView);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
@@ -45,6 +49,19 @@ public class ItemList extends BaseActivity {
 
         databaseService = DatabaseService.getInstance();
 
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterList(newText);
+                return true;
+            }
+        });
+
         loadItemsFromFirebase();
     }
     private void loadItemsFromFirebase() {
@@ -52,7 +69,11 @@ public class ItemList extends BaseActivity {
             @Override
             public void onCompleted(List<Item> items) {
                 dataList.clear();
+                allItems.clear();
+
+                allItems.addAll(items);
                 dataList.addAll(items);
+
                 adapter.notifyDataSetChanged();
             }
 
@@ -67,5 +88,22 @@ public class ItemList extends BaseActivity {
     }
     protected int getNavigationMenuItemId() {
         return R.id.nav_list_item;
+    }
+    private void filterList(String text) {
+        ArrayList<Item> filteredList = new ArrayList<>();
+
+        for (Item item : allItems) {
+            if (item.getName().toLowerCase().contains(text.toLowerCase())) {
+                filteredList.add(item);
+            }
+        }
+
+        dataList.clear();
+        if (filteredList.isEmpty()) {
+            // "No Data Found"
+        } else {
+            dataList.addAll(filteredList);
+        }
+        adapter.notifyDataSetChanged();
     }
 }
